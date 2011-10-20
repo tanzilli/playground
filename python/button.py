fb0f5c8fb32fe907a23adcbb6264a1f0b97e0ac2
Sergio Tanzilli <tanzilli@acmesystems.it>
2011-10-20 12:24:59 +0300

    Update python/button.py

--- python/button.py
+++ python/button.py
@@ -1,12 +1,10 @@
-#!/usr/bin/python
-import time
 import fox
+import time
  
 print "Pressing button"
 print "Type ctrl-C to exit"
  
-#led = fox.Pin('J7','3','low')
-led = fox.Pin('J6','21','low')
+led = fox.Pin('J7','3','low')
 button = fox.Pin('J7','5','in')
  
 while True:
