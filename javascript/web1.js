33074bec824e27622549006bd6678134d68e21fe
Sergio Tanzilli <tanzilli@acmesystems.it>
2011-10-20 16:01:31 +0200

    Added javascript directory

---
+++ javascript/web1.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Hello World\n');
+}).listen(81, "0.0.0.0");
+
+console.log('Server web running on FOX Board G20');
