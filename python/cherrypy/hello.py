1956f53ce88dfb55e76e04e6ad74e15a6d236d1e
Sergio Tanzilli <tanzilli@acmesystems.it>
2011-10-28 15:02:12 +0200

    Added CherryPy Hello World example

---
+++ python/cherrypy/hello.py
@@ -0,0 +1,11 @@
+import cherrypy
+
+class HelloWorld(object):
+    def index(self):
+        return "Hello World!"
+    index.exposed = True
+
+cherrypy.config.update({'server.socket_host': '0.0.0.0'})
+cherrypy.config.update({'server.socket_port': 81})
+cherrypy.quickstart(HelloWorld())
+
