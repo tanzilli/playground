ad41f617e962b25684339919ace85511f43a8152
Sergio Tanzilli <tanzilli@acmesystems.it>
2012-03-05 18:07:50 +0100

    sqlite example updated

--- python/sqlite/select.py
+++ python/sqlite/select.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python
+#http://www.acmesystems.it/sqlite
+
 import time
 from pysqlite2 import dbapi2 as sqlite
- 
-# Insert a new record for this sample on the SQLite database
  
 connection = sqlite.connect('/root/mydb.sqlite')
 cursor = connection.cursor()
 cursor.execute('SELECT * FROM tbl1')
+
+for row in cursor:
+	print row
+
 connection.commit()
+connection.close()
  
 
