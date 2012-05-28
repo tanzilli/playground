6a650cfa4ee0b4b3f966f87c7ddedd68871b44d8
root <root@debarm.(none)>
2012-05-28 13:57:36 +0200

    Added Daisy24 class

--- python/fox/fox.py
+++ python/fox/fox.py
@@ -11,7 +11,13 @@
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 
-import os.path
+try:
+	import os.path
+	import smbus
+	import time
+except:
+	pass
+
 
 serial_ports = {
 	'D1' :  '/dev/ttyS2',
@@ -488,6 +494,88 @@ class Daisy19():
 			return True
 		else:
 			return False
+
+
+
+class Daisy24():
+
+	"""
+	DAISY-24 (16x2 LCD module
+	http://www.acmesystems.it/?id=DAISY-24
+	"""
+
+	i2c_bus=-1
+	lcd_address = 0x3E
+
+	def __init__(self,bus_id):
+		self.i2c_bus = smbus.SMBus(bus_id)
+		self.sendcommand(0x38)
+		self.sendcommand(0x39)
+		self.sendcommand(0x14) #Internal OSC freq
+		self.sendcommand(0x72) #Set contrast 
+		self.sendcommand(0x54) #Power/ICON control/Contrast set
+		self.sendcommand(0x6F) #Follower control
+		self.sendcommand(0x0C) #Display ON
+		self.clear()
+		return
+
+	def sendcommand(self,value):
+		self.i2c_bus.write_byte_data(self.lcd_address,0x00,value)
+		return
+
+	def senddata(self,value):
+		self.i2c_bus.write_byte_data(self.lcd_address,0x40,value)
+		return
+
+	def clear(self):
+		"""
+		CLear the display content
+		"""
+		self.sendcommand(0x01)
+		time.sleep(0.001)
+		return
+
+	def setcontrast(self,value):
+		"""
+		Set the display contrast
+		value = 0 to 15
+		"""
+		self.sendcommand(0x70 + value)
+		return
+
+	def home(self):
+		"""
+		Place the curson at home position
+		"""
+		self.sendcommand(0x03)
+		time.sleep(0.001)
+		return
+
+	def curpos(self,x,y):
+		if y<0 or y>1:
+			return
+		if x<0 or x>15:
+			return
+
+		if y==0:
+			lcd.sendcommand(0x80+0x00+x)
+		else:
+			lcd.sendcommand(0x80+0x40+x)
+		return
+
+	def putchar(self,value):
+		self.senddata(value)
+		return
+
+	def putstring(self,string):
+		if len(string)==0:
+			return
+		if len(string)>16:
+			string=string[0:16]
+
+		for char in string:
+			self.putchar(ord(char))
+		return
 
 
 #--------------------------------------------------------------
