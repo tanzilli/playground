2b043e6d2070f0c9188ad92693c8866eff8f810d
Sergio Tanzilli <tanzilli@acmesystems.it>
2012-05-29 17:41:28 +0200

    Daisy22 example updated

--- python/daisy22/blink.py
+++ python/daisy22/blink.py
@@ -1,12 +1,17 @@
 import fox
 import time
 
-#P0 out on Daisy-22 
+#Define the istance P0 as a GPIO line 0
+#on a Daisy-22 with all the address dip switches  
+#at the OFF position (I2C address = 0x27)  
 
 i2c_bus=0
 i2c_address=0x27
  
 P0=fox.Daisy22(i2c_bus,i2c_address,0)
+
+#Turn on (1) and off (0) the GPIO line
+#each second
 
 while True:
 	P0.on()
