afe25cad537563c72ae8dee7ffc06e97c2d697e0
Sergio Tanzilli <tanzilli@acmesystems.it>
2012-05-30 08:46:48 +0200

    pulse.py example on Daisy-22

---
+++ python/daisy22/pulse.py
@@ -0,0 +1,33 @@
+import fox
+import time
+
+#Define which i2c_bus device driver to use
+#(normally /dev/i2c-0) 
+i2c_bus=0
+
+#Set I2C address configured on the Daisy-22
+i2c_address=0x27
+
+#Create an istance of Daisy-22 class 
+#to manage the P0 line
+ 
+P0=fox.Daisy22(i2c_bus,i2c_address,0)
+
+#Create an istance of Daisy-22 class 
+#to manage the P1 line
+ 
+P1=fox.Daisy22(i2c_bus,i2c_address,1)
+
+#Turn on (1) and off (0) the GPIO line
+#each second
+
+#Send a pulse on P0 gpio line 
+P0.on()
+time.sleep(1)
+P0.off()
+
+#Send a pulse on P1 gpio line 
+P1.on()
+time.sleep(1)
+P1.off()
+
