dc89c35f6ef5dc86aa2186e42be60d85368c3c30
Sergio Tanzilli <tanzilli@acmesystems.it>
2012-06-28 13:09:18 +0200

    Watchdog example updated

--- c/watchdog/watchdog.c
+++ c/watchdog/watchdog.c
@@ -9,13 +9,22 @@
 
 int main(void)
 {
-	int fd = open("/dev/watchdog", O_WRONLY);
 	int ret = 0;
+	int i,fd;
+
+	// The watchdog timer starts when /dev/watchdog is open
+	printf("Enable the watchdog timer\n");
+	printf("Press ctrl-C to simulate a program error\n");
+
+	fd = open("/dev/watchdog", O_WRONLY);
+
 	if (fd == -1) {
-		perror("watchdog");
+		perror("Watchdog disabled.");
 		exit(EXIT_FAILURE);
 	}
-	while (1) {
+
+	for (i=0;i<10;i++) {
+		printf("Clear the watchdog timer\n");
 		ret = write(fd, "\0", 1);
 		if (ret != 1) {
 			ret = -1;
@@ -23,6 +32,7 @@ int main(void)
 		}
 		sleep(10);
 	}
+	printf("Disable the watchdog before exit\n");
 	close(fd);
 	return ret;
 }
