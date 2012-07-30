dcbb0787df332191e9ca9bfc12e25575e308010e
Sergio Tanzilli <tanzilli@acmesystems.it>
2012-07-30 10:40:18 +0200

    added ablib module (ex acmelib, ex fox)

---
+++ ablib/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(name='ablib',
+	version='1.0',
+	description='Python module to manage the Acme Board devices',
+	author='Sergio Tanzilli',
+	author_email='tanzilli@acmesystems.it',
+	url='http://www.acmesystems.it',	
+	py_modules=['acmeboards']
+	)
+
+
+
