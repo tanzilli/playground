d02a934fb07828c72b764a0629cd501d26082153
Sergio Tanzilli <tanzilli@acmesystems.it>
2013-04-10 15:24:10 +0200

    Led on/off example using Tornado

---
+++ python/tornado/led/led.py
@@ -0,0 +1,27 @@
+import tornado.ioloop
+import tornado.web
+import ablib
+
+class ledon(tornado.web.RequestHandler):
+	def get(self):
+		print "Led ON"
+		led.on() 
+
+class ledoff(tornado.web.RequestHandler):
+	def get(self):
+		print "Led OFF"
+		led.off() 
+
+application = tornado.web.Application([
+	(r"/ledon", ledon),
+	(r"/ledoff", ledoff),
+	(r"/(.*)", tornado.web.StaticFileHandler, {"path": ".","default_filename": "index.html"}),
+])
+
+if __name__ == "__main__":
+	led = myled = ablib.Daisy11('D11','L1')
+
+	application.listen(8080,"0.0.0.0")
+	tornado.ioloop.IOLoop.instance().start()
+
+
