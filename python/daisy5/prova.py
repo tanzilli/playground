e09184a877238ced0b8cbbf337a64376fe4e2d79
Sergio Tanzilli <tanzilli@acmesystems.it>
2013-04-30 19:07:19 +0200

    poll test

--- python/daisy5/prova.py
+++ python/daisy5/prova.py
@@ -3,18 +3,37 @@
 # Test interrupts.
 
 import select, time, ablib
+import thread
+import threading
+
+def check_button(fd,callback):
+	po = select.epoll()
+	po.register(fd,select.EPOLLET)
+	while True:
+		events = po.poll()
+		callback()
+
+def tasto_P1():
+	print "Tasto P1 premuto"
+
+def tasto_P2():
+	print "Tasto P2 rilasciato"
 
 P1=ablib.Daisy5('D11','P1')
-P1.set_edge("both")
+P1.set_edge("rising")
 
-#while True:
-#	if (P1.pressed()):
-#		print "P1 pressed" 
+P2=ablib.Daisy5('D11','P2')
+P2.set_edge("falling")
 
-po = select.epoll()
-po.register(P1.fd,select.EPOLLET)
 
-t1 = time.time()
+thread.start_new_thread(check_button,(P1.fd,tasto_P1))
+thread.start_new_thread(check_button,(P2.fd,tasto_P2))
+
+
+i=0
 while True:
-	events = po.poll()
-	print "falling edge"
+	time.sleep(1)
+	print i
+	i=i+1
+	pass
+
