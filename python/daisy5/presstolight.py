6a588a9d9cefefbfb801ef24dd7c9c2a8f0de3c0
Sergio Tanzilli <tanzilli@acmesystems.it>
2013-05-03 16:21:34 +0200

    Daisy5 example on Terra board

--- python/daisy5/presstolight.py
+++ python/daisy5/presstolight.py
@@ -3,36 +3,39 @@ import time
 import ablib
  
 # Create a list of led istances on Daisy-11
-# Wire the Daisy-11 board on D2
-# Wire the Daisy-5 board on D5
+
+connector_buttons="D11"
+connector_leds="D12"
+
+button = [
+	ablib.Daisy5(connector_buttons,'P1'),
+	ablib.Daisy5(connector_buttons,'P2'),
+	ablib.Daisy5(connector_buttons,'P3'),
+	ablib.Daisy5(connector_buttons,'P4'),
+	ablib.Daisy5(connector_buttons,'P5'),
+	ablib.Daisy5(connector_buttons,'P6'),
+	ablib.Daisy5(connector_buttons,'P7'),
+	ablib.Daisy5(connector_buttons,'P8'),
+]
 
 led = [
-	ablib.Daisy11('D2','L1'),
-	ablib.Daisy11('D2','L2'),
-	ablib.Daisy11('D2','L3'),
-	ablib.Daisy11('D2','L4'),
-	ablib.Daisy11('D2','L5'),
-	ablib.Daisy11('D2','L6'),
-	ablib.Daisy11('D2','L7'),
-	ablib.Daisy11('D2','L8'),
+	ablib.Daisy11(connector_leds,'L1'),
+	ablib.Daisy11(connector_leds,'L2'),
+	ablib.Daisy11(connector_leds,'L3'),
+	ablib.Daisy11(connector_leds,'L4'),
+	ablib.Daisy11(connector_leds,'L5'),
+	ablib.Daisy11(connector_leds,'L6'),
+	ablib.Daisy11(connector_leds,'L7'),
+	ablib.Daisy11(connector_leds,'L8'),
 ]
  
-button = [
-	ablib.Daisy5('D5','P1'),
-	ablib.Daisy5('D5','P2'),
-	ablib.Daisy5('D5','P3'),
-	ablib.Daisy5('D5','P4'),
-	ablib.Daisy5('D5','P5'),
-	ablib.Daisy5('D5','P6'),
-	ablib.Daisy5('D5','P7'),
-	ablib.Daisy5('D5','P8'),
-]
+
 
 # Forever loop
 while True:
 	# Scan each button and turn the relative led
 	for i in range (0,8):      
-		if button[i].pressed(): 
+		if button[i].get(): 
 			led[i].on() 
 		else: 
 			led[i].off()
--- python/daisy5/read.py
+++ python/daisy5/read.py
@@ -1,40 +1,35 @@
-#!/usr/bin/python
-
-#Read the state of each button on Daisy-5
-#when wired on D5 connecor
-
-#http://www.acmesystems.it/DAISY-5
-
-import time
 import ablib
+
+connector="D11"
 
 # Create an istance for each button
 
-P1 = ablib.Daisy5('D5','P1')
-P2 = ablib.Daisy5('D5','P2')
-P3 = ablib.Daisy5('D5','P3')
-P4 = ablib.Daisy5('D5','P4')
-P5 = ablib.Daisy5('D5','P5')
-P6 = ablib.Daisy5('D5','P6')
-P7 = ablib.Daisy5('D5','P7')
-P8 = ablib.Daisy5('D5','P8')
+P1 = ablib.Daisy5(connector,'P1')
+P2 = ablib.Daisy5(connector,'P2')
+P3 = ablib.Daisy5(connector,'P3')
+P4 = ablib.Daisy5(connector,'P4')
+P5 = ablib.Daisy5(connector,'P5')
+P6 = ablib.Daisy5(connector,'P6')
+P7 = ablib.Daisy5(connector,'P7')
+P8 = ablib.Daisy5(connector,'P8')
 
+# Poll on each butto to check if pressed
 while True:
-	if (P1.pressed()):
+	if (P1.get()):
 		print "P1 pressed" 
-	if (P2.pressed()):
+	if (P2.get()):
 		print "P2 pressed" 
-	if (P3.pressed()):
+	if (P3.get()):
 		print "P3 pressed" 
-	if (P4.pressed()):
+	if (P4.get()):
 		print "P4 pressed" 
-	if (P5.pressed()):
+	if (P5.get()):
 		print "P5 pressed" 
-	if (P6.pressed()):
+	if (P6.get()):
 		print "P6 pressed" 
-	if (P7.pressed()):
+	if (P7.get()):
 		print "P7 pressed" 
-	if (P8.pressed()):
+	if (P8.get()):
 		print "P8 pressed" 
 
 
