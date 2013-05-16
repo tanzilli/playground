9666a99fa064f314c9218295a81cbc83dff537ad
Sergio Tanzilli <tanzilli@acmesystems.it>
2013-05-16 14:11:29 +0200

    SQLite examples

---
+++ python/sqlite/create.sql
@@ -0,0 +1,3 @@
+create table events(timestamp text, description text);
+insert into events values(datetime("now"),"First test event");
+insert into events values(datetime("now"),"Second test event");
--- python/sqlite/events.py
+++ python/sqlite/events.py
@@ -3,10 +3,12 @@ import sqlite3
  
 connection = sqlite3.connect('events.sqlite')
 cursor = connection.cursor()
-cursor.execute('SELECT * FROM events;')
+cursor.execute("SELECT strftime('%d/%m/%Y %H:%M:%S',timestamp),description FROM events;")
 
 for row in cursor:
-	print row
+	print "  Timestamp:",row[0]
+	print "Description:",row[1]
+	print " "	
 
 connection.commit()
 connection.close()
