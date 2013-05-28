c92b3fcd6cee86b200336574e1b65a941651979b
Sergio Tanzilli <tanzilli@acmesystems.it>
2013-05-28 17:22:30 +0200

    Updated Tornado adc meter example

--- python/tornado/adc/adc.py
+++ python/tornado/adc/adc.py
@@ -4,7 +4,7 @@ import tornado.ioloop
 import tornado.web
 import tornado.websocket
 import tornado.gen
-import json
+import ablib
 from tornado.options import define, options
 
 clients = []
@@ -19,32 +19,34 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
 	def on_message(self, message):
 		print "tornado received from client: %s" % message
 		self.write_message(u"OK: " + message)
-		#....
+
 	def on_close(self):
 		print 'connection closed'
 		clients.remove(self)		
 
 def main():
+	adc=ablib.Daisy20()
+
 	application = tornado.web.Application([
 		(r"/websocket", WebSocketHandler),
 		(r"/(.*)", tornado.web.StaticFileHandler, {"path": ".","default_filename": "index.html"}),
 	])
 	application.listen(8080)
  
-	def batteryStatus():
+	def adcReader():
 		global batteryValue
+		
 		if(batteryValue > 0):
 			batteryValue -= 0.1
 		else:
 			batteryValue=10
 
-		json_string="{'ch0':%.2f,'ch1':2,'ch2':3,'ch3':4}" % batteryValue
+		json_string="{'ad0':%.2f,'ad1':%.2f,'ad2':%.2f,'ad3':%.2f}" % (adc.get(0),adc.get(1),adc.get(2),adc.get(3))
 		for c in clients:
 			c.write_message(json_string)
-			print json_string
 
 	mainLoop = tornado.ioloop.IOLoop.instance()
-	scheduler = tornado.ioloop.PeriodicCallback(batteryStatus, 100, io_loop = mainLoop)
+	scheduler = tornado.ioloop.PeriodicCallback(adcReader, 100, io_loop = mainLoop)
 	scheduler.start()
 	mainLoop.start()
 	
