48f77ad75066bd378b23292f863d40943615a26e
Sergio Tanzilli <tanzilli@acmesystems.it>
2013-07-10 08:19:55 +0200

    Base for microsd test with mysql

---
+++ test/mysql/microsdtest.sql
@@ -0,0 +1,17 @@
+/*DROP TABLE IF EXISTS `microsdtest`;*/
+CREATE IF NOT EXISTS TABLE `microsdtest` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `field1` varchar(255) DEFAULT NULL,
+  `field2` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `microsdtest` WRITE;
+
+INSERT INTO `microsdtest` (field1,field2) VALUES 
+('Field 1 Record 1','Field 2 Record 1'),
+('Field 1 Record 2','Field 2 Record 2'),
+('Field 1 Record 3','Field 2 Record 3'),
+('Field 1 Record 4','Field 2 Record 4')
+
+UNLOCK TABLES;
