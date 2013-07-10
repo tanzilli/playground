57aeff7f918194621c2441c66f381b3717639ffb
Sergio Tanzilli <tanzilli@acmesystems.it>
2013-07-10 11:33:08 +0200

    Fixes on mysql test

--- test/mysql/microsdtest.py
+++ test/mysql/microsdtest.py
@@ -1,11 +1,19 @@
 import MySQLdb
+import time
 
 db=MySQLdb.connect(host="127.0.0.1",port=3306,db="mydb",user="root",passwd="ariag25")
-
 cur = db.cursor()
-cur.execute("SELECT * FROM microsdtest;")
 
-rows = cur.fetchall()
+while True:
+	for i in range (0,10):
+		query="INSERT INTO microsdtest (field1,field2) VALUES ('Field 1 Record %d','Field 2 Record %d');" % (i,i)
+		cur.execute(query)
+		db.commit()
 
-for row in rows:
-	print row
+	query="DELETE FROM microsdtest LIMIT 9;"
+	cur.execute(query)
+	db.commit()
+	
+	
+
+
--- test/mysql/microsdtest.sql
+++ test/mysql/microsdtest.sql
@@ -1,5 +1,5 @@
 /*DROP TABLE IF EXISTS `microsdtest`;*/
-CREATE IF NOT EXISTS TABLE `microsdtest` (
+CREATE TABLE IF NOT EXISTS  `microsdtest` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `field1` varchar(255) DEFAULT NULL,
   `field2` varchar(255) DEFAULT NULL,
@@ -12,6 +12,6 @@ INSERT INTO `microsdtest` (field1,field2) VALUES
 ('Field 1 Record 1','Field 2 Record 1'),
 ('Field 1 Record 2','Field 2 Record 2'),
 ('Field 1 Record 3','Field 2 Record 3'),
-('Field 1 Record 4','Field 2 Record 4')
+('Field 1 Record 4','Field 2 Record 4');
 
 UNLOCK TABLES;
