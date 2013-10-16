04e801c3f8902d1659d7bda04df45ff5e94dad6f
Sergio Tanzilli <tanzilli@acmesystems.it>
2013-10-16 17:31:17 +0200

    Example updated

--- python/terra/usb_power.py
+++ python/terra/usb_power.py
@@ -1,32 +1,32 @@
-import time
-import ablib
+from time import sleep
+from ablib import Pin
  
-Power_USB_A = ablib.Pin('N','7','high')
-Power_USB_B = ablib.Pin('N','8','high')
-Power_USB_C = ablib.Pin('N','9','high')
+Power_USB_A = Pin('N7','HIGH')
+Power_USB_B = Pin('N8','HIGH')
+Power_USB_C = Pin('N9','HIGH')
 
 while True:
 	print "USB A OFF"
 	Power_USB_A.off()
-	time.sleep(1) 
+	sleep(1) 
 
 	print "USB B OFF"
 	Power_USB_B.off()
-	time.sleep(1) 
+	sleep(1) 
 
 	print "USB C OFF"
 	Power_USB_C.off()
-	time.sleep(1) 
+	sleep(1) 
 
 	print "USB A ON"
 	Power_USB_A.on()
-	time.sleep(1) 
+	sleep(1) 
 
 	print "USB B ON"
 	Power_USB_B.on()
-	time.sleep(1) 
+	sleep(1) 
 
 	print "USB C ON"
 	Power_USB_C.on()
-	time.sleep(1) 
+	sleep(1) 
 
